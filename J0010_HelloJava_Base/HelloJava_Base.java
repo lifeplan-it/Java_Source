4fb976db3342910153020f27d15f6016a82c06e6
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-13 12:47:03 +0900

    New File Add J0000 J0010

---
+++ J0010_HelloJava_Base/HelloJava_Base.java
@@ -0,0 +1,10 @@
+// HelloJava_Base.java
+// 文字列[HelloJava]を標準出力する
+// メインクラスの宣言
+class HelloJava_Base {
+	//メインメソッドの宣言 
+	public static void main(String[] args)  {
+		//文の記述 
+		System.out.println("Hello JAVA!!") ;
+    }
+}
