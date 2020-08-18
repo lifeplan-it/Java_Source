fa5e41ee70d2095e2462186ff4dfa1dadc432148
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-18 15:47:47 +0900

    Hellojava Main

---
+++ J0050_HelloJava_Gui_Base/HelloJava_Gui_Base.java
@@ -0,0 +1,73 @@
+// HelloJava_Gui_Main.java
+// 概要
+// 　　Frameに文字を出力するプログラム
+
+// GUI用ライブラリの利用の定義
+import java.awt.*;
+// GUI用イベントライブラリの定義
+import java.awt.event.*;
+
+// メインクラス
+class HelloJava_Gui_Main
+{
+  // メインメソッド
+  public static void main(String[] args)
+  {
+	HelloJava_Gui_Method () ;
+  }
+
+  // 処理メソッド
+  public static void HelloJava_Gui_Method ()
+  {
+      // クラスのオブジェクト化        
+      HelloJava_Gui_subClass obj = new HelloJava_Gui_subClass ();
+      // 引数の初期化
+      String str = "Hello JAVA!!" ;
+      //  オブジェクト化したメソッドの実行
+      obj.HelloJava_Gui_subClass_method ( str ) ;
+  }
+}
+
+// ライブラリのクラスを使ったサブクラスの作成
+// HelloJava_Gui_subClass.java
+// クラスの定義
+// Frameクラスを継承している。
+class HelloJava_Gui_subClass extends Frame
+{
+  // インスタンス変数の定義
+  private Label lb;
+  // コンストラクタの定義
+  public void HelloJava_Gui_subClass ( )
+  {
+     // このクラスでは何もしない
+     // this.super　( "Hello JAVA" ) ;
+  }
+
+ // メインクラスから呼び出されるメソッド
+  public void HelloJava_Gui_subClass_method (String str )
+  {
+      setTitle ( "Hello JAVA" );			// タイトル定義
+      setBounds(200, 200, 100, 80);		// Frameのサイズの定義
+      setSize(480, 240);			// Frameのサイズの定義
+      setLayout(null) ; 				// レイアウトマネージャーの定義
+      setVisible(true);				// 表示の指定
+
+      lb = new Label( str );			// ラベルの定義（引数　str ）を使用
+      lb.setBounds(10, 40, 250,  20);		// ラベルの位置定義
+      lb.setForeground(Color.BLUE);		// 文字の色
+      lb.setFont(new Font("Serif", Font.BOLD, 24));	// 表示Fontの設定
+
+      add(lb);				// FrameにLabelを追加
+      addWindowListener(new GuiWindowListener());	// イベントリスナの定義
+  }
+
+   // イベントの処理
+   class GuiWindowListener extends WindowAdapter
+   {
+      // Close イベントの取得
+      public void windowClosing(WindowEvent e)
+      {
+         System.exit(0);				// 終了
+      }
+   }
+}
---
+++ J0050_HelloJava_Gui_Base/README.md
@@ -0,0 +1,10 @@
+１）メインクラスのメインメソッドは、
+　　メソッド「HelloJava_Gui_Method」を呼び出しています。
+２）メソッド「HelloJava_Gui_Method」は、
+　　サブクラス「HelloJava_Gui_subClass」をインスタンス化しています。
+３）インスタンス化する事で、
+　　サブクラスの中のメソッド「HelloJava_Gui_subClass_method」を呼び出しています。
+４）サブクラス「HelloJava_Gui_subClass」は
+　　Frameクラスのメソッドを継承しています。
+５）サブクラスの中のメソッド「HelloJava_Gui_subClass_method」は
+　　Frameクラスのメソッドを使ってGUI出力の処理を行っています。
