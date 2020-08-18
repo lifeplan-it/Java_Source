7582ebc5a2ca68c5f3264835547c3b628ff2f3e0
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-18 13:47:19 +0900

    GUI

--- J0050_HelloJava_Gui_Main/HelloJava_Gui_Main.java
+++ J0050_HelloJava_Gui_Main/HelloJava_Gui_Main.java
@@ -1,54 +1,84 @@
 // HelloJava_Gui_Main.java
 // 概要
 // 　　Frameに文字を出力するプログラム
-
 // GUI用ライブラリの利用の定義
 import java.awt.*;
 // GUI用イベントライブラリの定義
 import java.awt.event.*;
 
 // メインクラス
-class HelloJava_Gui_Main  {
+class HelloJava_Gui_Main
+{
   // メインメソッド
-  public static void main(String[] args) {
-	HelloJava_Gui_Method () ;
+  public static void main(String[] args)
+  {
+	   HelloJava_Gui_Method () ;
   }
-  public static void HelloJava_Gui_Method () {		// GUI用メソッド
-  	HelloJava_Gui_subClass obj 
-	     = new HelloJava_Gui_subClass ();		// クラスのオブジェクト化        
 
-	String str = "Hello JAVA!!" ;			// 引数の初期化
-	obj.HelloJava_Gui_subClass_method ( str ) ;	//  オブジェクト化したクラスの実行
+  // GUI用メソッド
+  public static void HelloJava_Gui_Method ()
+  {
+    // クラスのオブジェクト化
+  	HelloJava_Gui_subClass obj
+	     = new HelloJava_Gui_subClass ();
+
+    //  オブジェクト化したメソッドの実行
+	  String str = "Hello JAVA!!" ;
+    obj.HelloJava_Gui_subClass_method ( str ) ;
   }
 }
+
 // HelloJava_Gui_subClass.java
-class HelloJava_Gui_subClass extends Frame {		// クラスの定義
-						// Frameクラスを継承している。
-  private Label lb;					// インスタンス変数の定義
-  public void HelloJava_Gui_subClass ( )  {		// コンストラクタの定義
-	// this.super　( "Hello JAVA" ) ;			// このクラスでは何もしない
+// クラスの定義
+// Frameクラスを継承している。
+class HelloJava_Gui_subClass extends Frame
+{
+  // インスタンス変数の定義
+  private Label lb;
+  // コンストラクタの定義
+  public void HelloJava_Gui_subClass ( )
+  {
+      // このクラスでは何もしない
+	    // this.super　( "Hello JAVA" ) ;
   }
 
-  public void HelloJava_Gui_subClass_method (String str )  { // メインクラスから呼び出されるメソッド
+  // メインクラスから呼び出されるメソッド
+  public void HelloJava_Gui_subClass_method (String str )
+  {
+    // タイトル定義
+    setTitle ( "Hello JAVA" );
+    // Frameのサイズの定義
+	  setBounds(200, 200, 100, 80);
+    // Frameのサイズの定義
+    setSize(480, 240);
+    // レイアウトマネージャーの定義
+    setLayout(null) ;
+    // 表示の指定
+    setVisible(true);
 
-	setTitle ( "Hello JAVA" );			// タイトル定義
-	setBounds(200, 200, 100, 80);			// Frameのサイズの定義
-	setSize(480, 240);				// Frameのサイズの定義
-	setLayout(null) ; 				// レイアウトマネージャーの定義
-	setVisible(true);				// 表示の指定
+    // ラベルの定義（引数　str ）を使用
+    lb = new Label( str );
+    // ラベルの位置定義
+    lb.setBounds(10, 50, 250,  20);
+    // 文字の色
+	  lb.setForeground(Color.BLUE);
+    // 表示Fontの設定
+    lb.setFont(new Font("Serif", Font.BOLD, 24));
 
-	lb = new Label( str );			// ラベルの定義（引数　str ）を使用
-	lb.setBounds(10, 50, 250,  20);		// ラベルの位置定義
-	lb.setForeground(Color.BLUE);		// 文字の色
-	lb.setFont(new Font("Serif", Font.BOLD, 24));	// 表示Fontの設定
-
-	add(lb);					// FrameにLabelを追加
-	addWindowListener(new GuiWindowListener());	// イベントリスナの定義
+    // FrameにLabelを追加
+	   add(lb);
+     // イベントリスナの定義
+	   addWindowListener(new GuiWindowListener());
   }
 
-   class GuiWindowListener extends WindowAdapter  {	// イベントの処理
-      public void windowClosing(WindowEvent e) {		// Close イベントの取得
-         System.exit(0);					// 終了
+   // イベントの処理
+   class GuiWindowListener extends WindowAdapter
+   {
+      // Close イベントの取得
+      public void windowClosing(WindowEvent e)
+      {
+        // 終了
+        System.exit(0);
       }
    }
 }
