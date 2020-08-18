5af7fc6124523f3638d0f90590a7bbbec694bc37
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-18 20:08:33 +0900

    Print

--- J0120 Output_Escapesequence/Output_Escapesequence.java
+++ J0120 Output_Escapesequence/Output_Escapesequence.java
@@ -1,5 +1,5 @@
 // Output_Escapesequence.java
-// エスケープシーケンス	意味
+// エスケープシーケンス
 // ¥b	バックスペース
 // ¥t	水平タブ
 // ¥n	改行
@@ -10,3 +10,49 @@
 // ¥¥	¥文字
 // ¥ooo	8進数の文字コードが表す文字
 // ¥uhhhh	16進数の文字コードが表す文字
+
+class Output_Escapesequence
+{
+  public static void main(String[] args)  {
+	// リテラルの出力
+	Output_Print_Literal ();
+	Output_Print_Add () ;
+	Output_Print_Esc () ;
+	Output_Print_Str () ;
+	Output_Print_Esc () ;
+	Output_Print_Tab ();
+   }
+
+   private static void Output_Print_Literal ()   {
+	// リテラルの出力
+      	System.out.println("ようこそJavaへ!");
+   }
+
+   private static void Output_Print_AddStr () {
+	// リテラルの結合
+	System.out.println("文字を" + "+" + "で" + "連結する");
+    }
+ 
+   private static void Output_Print_NoLf () {
+	// 改行しないで出力する
+	System.out.print("printは");
+	System.out.print("改行");
+	System.out.print("しない");
+	System.out.println("");
+    }
+
+    private static void Output_Print_Esc () {
+	// 改行文字（\n）
+	System.out.println("行を\n強制的に\n改行");
+    }
+
+    private static void Output_Print_Tab () {
+	// TAB字（\t）
+	System.out.print("TAB\t");
+	System.out.print("で\t");
+	System.out.print("間隔を\t");
+	System.out.print("空ける\t");
+	System.out.println("");
+    }
+
+}
