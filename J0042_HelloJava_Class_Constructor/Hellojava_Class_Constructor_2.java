663ffb731e140e633abcaa198165ae55d4fb4d42
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-18 13:02:28 +0900

    Constructor

---
+++ J0042_HelloJava_Class_Constructor/Hellojava_Class_Constructor_2.java
@@ -0,0 +1,19 @@
+// Hellojava_Class_Constructor_2.java
+// 概要
+// コンストラクタを使ったHello Java_2
+//
+// メインクラス
+class Hellojava_Class_Constructor_2 {
+  // コンストラクタ
+  public Hellojava_Class_Constructor_2 ()
+  {
+    System.out.println( "Hello Java" );
+  }
+
+  // メインメソッド
+  public static void main ( String[] args )
+  {
+    // 自分をインスタンス化
+    new Hellojava_Class_Constructor_2 () ;
+  }
+}
