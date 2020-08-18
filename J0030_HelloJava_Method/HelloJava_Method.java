d77b5f7ad1061f2b92cf5e2150443a907d228486
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-18 13:19:57 +0900

    Clon Blog

---
+++ J0030_HelloJava_Method/HelloJava_Method.java
@@ -0,0 +1,19 @@
+// HelloJava_Method.java
+// 概要
+// メソッドを使ったHelloJavaです。
+//
+// メインクラス
+class HelloJava_Method {
+    // メインメソッド
+    public static void main(String[] args)
+    {
+        // メソッド呼び出し
+        HelloJava_Print () ;
+    }
+    // メソッド:HelloJava_Method
+    public static void HelloJava_Print ()
+    {
+        // 処理：文字の出力
+        System.out.println("HelloJAVA");
+    }
+}
