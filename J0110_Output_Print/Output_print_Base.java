ad4a0a0bb81eb52cfdb455d42075b21adf8a6177
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-19 17:28:26 +0900

    Edit Output

--- J0110_Output_Print/Output_print_Base.java
+++ J0110_Output_Print/Output_print_Base.java
@@ -1,43 +1,50 @@
 // Output_print_Base.java
-class Output_print_Base  {
-  public static void main(String[] args)  {
-	Output_Print_String ();
-	Output_Print_Str () ;
-	Output_Print_Add () ;
-	Output_Print_Esc () ;
-	Output_Print_Tab ();
-   }
+// 標準出力のサンプルプログラムです。
+//
+// 数　値のリテラル出力をするメソッド
+//    void Output_Numerical ()
+// 文字列のリテラル出力をするメソッド
+//    void Output_String_lf ()
+// 改行しないで文字列のリテラルの出力をするメソッド
+//    void Output_String
+// 文字列のリテラルを結合して出力するメソッド
+//    void Output_Print_Add
+// メインクラス
+class Output_print_Base {
+  // メインメソッド
+  public static void main(String[] args) {
+    // 各メソッドの呼び出し
+    // 数　値のリテラル出力をするメソッド
+    Output_Numerical();
+    // 文字列のリテラル出力をするメソッド
+	  Output_String_lf ();
+    // 改行しないで文字列のリテラル出力をするメソッド
+    Output_String ();
+    // 文字列のリテラルを結合して出力するメソッド
+    Output_Add () ;
+	}
 
-   private static void Output_Print_String () {
-	// リテラルの出力
-      	System.out.println("ようこそJavaへ!");
-   }
+  // 数　値のリテラル出力をするメソッド
+  private static void Output_Numerical () {
+	  System.out.println("ようこそJavaへ!");
+  }
 
-    private static void Output_Print_Str () {
-	// 改行しないで出力する
-	System.out.print("printは");
-	System.out.print("改行");
-	System.out.print("しない");
-	System.out.println("");
-    }
+  // 文字列のリテラル出力をするメソッド
+  private static void Output_String_lf () {
+    System.out.println("ようこそ");
+    System.out.println("Javaへ!");
+  }
 
-    private static void Output_Print_Add () {
-	// リテラルの結合
-	System.out.println("文字を" + "+" + "で" + "連結する");
-    }
+  // 改行しないで文字列のリテラル出力をするメソッド
+  private static void Output_String ()  {
+    System.out.print("printは");
+	  System.out.print("改行");
+	  System.out.print("しない");
+	  System.out.println("");
+  }
 
-    private static void Output_Print_Esc () {
-	// 改行文字（\n）
-	System.out.println("行を\n強制的に\n改行");
-    }
-
-    private static void Output_Print_Tab () {
-	// TAB字（\t）
-	System.out.print("TAB\t");
-	System.out.print("で\t");
-	System.out.print("間隔を\t");
-	System.out.print("空ける\t");
-	System.out.println("");
-    }
-
+  // 文字列のリテラルを結合して出力するメソッド
+  private static void Output_Add ()  {
+	   System.out.println("文字を" + "+" + "で" + "連結する");
+  }
 }
