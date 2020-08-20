f1873b7fbd805d5f5a73a4eb9e9e151c59ec6ccd
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-20 16:02:13 +0900

    update Esc

--- J0120 Output_Escapesequence/Output_Escapesequence.java
+++ J0120 Output_Escapesequence/Output_Escapesequence.java
@@ -1,34 +1,48 @@
-// Output_Escapesequence.java
-// エスケープシーケンス出力
-// 英数以外の文字を出力したい時に使用します。
-// 主なエスケープシーケンス
-// \b BS     バックスペース(Back Space)
-// \t  TAB	水平タブ(TAB)
-// \n LF      改行する(Line Feed)
-// \r CR      文頭に戻す(Carriage Return)
-// \f CLS 	改ページ(Page Feed)
-// \' 	シングルクオーテーション(Single Quotation)
-// \"	ダブルクオーテーション(Double Quotation)
-// \\	\文字(Back Slash)
-//
-//  サンプルメソッド
-// 改行（\n）を出力するメソッド
-//   void Output_LF ()
-// TAB（\t）を出力するメソッド
-//      void Output_TAB () {
-// '（\'）を出力するメソッド
-//      void Output_SQuotation ()
-// "（\"）を出力するメソッド
-//      void Output_WQuotation ()
-// \ \\ を出力するメソッド
-//      void Output_BackSlash () {
+/*
+# Java エスケープシーケンス出力
+# 解説
+ 英数以外の文字を出力したい時に使用します。
+
+# 主なエスケープシーケンス
+|記述|略号|内容|
+|:----:|:---|:-----:|
+| \b | BS |  バックスペース(Back Space) |
+| \t | TAB |	水平タブ(TAB) |
+| \n | LF  | 改行(Line Feed) |
+| \r | CR  | 文頭に戻す(Carriage Return) |
+| \f | CLS | 改ページ(Page Feed) |
+| \' | '   | シングルクオーテーション(Single Quotation) |
+| \" | "   | ダブルクオーテーション(Double Quotation) |
+| \\ | \   | バックスラッシュ(Back Slash) |
+
+# ソースファイル
+  Output_Escapesequence.java
+# クラス
+　Output_Escapesequence　　
+# メソッド
+## 改行（\n）を出力するメソッド
+      void Output_LF ()
+## TAB（\t）を出力するメソッド
+      void Output_TAB ()
+## '（\'）を出力するメソッド
+      void Output_SQuotation ()
+## "（\"）を出力するメソッド
+      void Output_WQuotation ()
+## \ \\ を出力するメソッド
+      void Output_BackSlash ()
+*/
 
 class Output_Escapesequence {
   public static void main(String[] args)  {
+      // 改行（\n）を出力するメソッド
       Output_LF () ;
+      // TAB（\t）を出力するメソッド
       Output_TAB ();
+      // '（\'）を出力するメソッド
       Output_SQuotation () ;
+      // "（\"）を出力するメソッド
       Output_WQuotation () ;
+      // \ \\ を出力するメソッド
       Output_BackSlash () ;
   }
 
--- J0230_Value_Map/Variable_Map_Base.java
+++ J0230_Value_Map/Variable_Map_Base.java
@@ -1,46 +1,68 @@
 // Java Mapの使い方
+// 基本構文
 // Map の宣言
-// Map<キーの型名, 値の型名> オブジェクト名 = new HashMap<>();
+//   Map<キーの型名, 値の型名> オブジェクト名 = new HashMap<>();
+//
 // Map 要素を追加
-// Map型オブジェクト名.put(キー, 値);
+//   Map型オブジェクト名.put(キー, 値);
+//
 // Map 要素を取得
-// Map型オブジェクト名.get(キー);
+//   Map型オブジェクト名.get(キー);
+//
 // String型を指定する
-// Map<String, String> map = new HashMap<String, String>();
-// Listを指定する
+//   Map<String, String> map = new HashMap<String, String>();
+//
+// MAPにListを指定する
 // Map<String, List<String>> map = new HashMap<>();
-// for(型 変数名 : List名){
-//    System.out.println(変数名);
-// }
-
+//
+//クラス説明
+// ソースファイル名
+// 　Variable_Map_Base.Java
+//メインクラス名
+// 　Variable_Map_Base
+//メソッド
+//  Mapの宣言
+//    Map<String, String> map = new HashMap<String, String>();
+//  Mapの初期化を行うメソッド
+//    Variable_Map_Init ( Map<String, String> )
+//  MAPの表示を行うメソッド
+//    Variable_Map_Print_Val ( Map<String, String> )
+//  Mapのキーを表示するメソッド
+//    Variable_Map_Print_Key ( Map<String, String> )
+//
 // Variable_Map_Base.java
 import java.util.*;
 public class Variable_Map_Base  {
-    public static void main(String[] args) {
-        // Mapの宣言
-        Map<String, String> map = new HashMap<String, String>();
+  // メインメソッド
+  Public static void main(String[] args)  {
+     // Mapの宣言
+     Map<String, String> map = new HashMap<String, String>();
+     // Mapの初期化を行うメソッド
+     Variable_Map_Init (map ) ;
+     // MAPの表示を行うメソッド
+     Variable_Map_Print_Val (map ) ;
+     // Mapのキーを表示するメソッド
+     Variable_Map_Print_Key (map ) ;
+  }
 
-        Variable_Map_Init (map ) ;
-        Variable_Map_Print_Val (map ) ;
-        Variable_Map_Print_Key (map ) ;
-
-    }
+    // Mapの初期化を行うメソッド
     public static void Variable_Map_Init (Map<String, String> map ) {
         map.put("key1", "apple");
         map.put("key2", "orange");
         map.put("key3", "melon");
     }
 
+    // MAPの表示を行うメソッド
     public static void Variable_Map_Print_Val (Map<String, String> map ) {
-	for( String val : map.values() ) {
-		System.out.println( val );
-	}
-    }
+　　　　for( String val : map.values() ) {
+　　　　　　System.out.println( val );
+	　}
+  }
 
-    public static void Variable_Map_Print_Key (Map<String, String> map ) {
-	for( String key : map.keySet () ) {
-		System.out.println( key );
-	}
-    }
+  // Mapのキーを表示するメソッド
+  public static void Variable_Map_Print_Key (Map<String, String> map ) {
+	  for( String key : map.keySet () ) {
+		  System.out.println( key );
+	  }
+  }
 }
-
