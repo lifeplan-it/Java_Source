b51a4471c0505b4a60862cbed0afd06908b05cd9
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-20 20:39:00 +0900

    Update Hellojava_Class_Base.java

--- J0040_HelloJava_Class/Hellojava_Class_Base.java
+++ J0040_HelloJava_Class/Hellojava_Class_Base.java
@@ -1,25 +1,35 @@
 // Hellojava_Class_Base.java
-class Hellojava_Class_Base {		// メインクラス
-   public static void main(String[] args)   {	// メインメソッド
-      Hellojava_Class_method()  ;　	// メソッド呼び出し
+// メインクラス
+class Hellojava_Class_Base {
+  // メインメソッド
+   public static void main(String[] args)
+   {
+      // メソッド呼び出し
+      Hellojava_Class_method() ;
    }
 
-   public static void Hellojava_Class_method()   {	// メソッド
-      Hellojava_Class_sub Obj1 = new Hellojava_Class_sub() ; 	// Hellojava_Class_subをオブジェクト化
-      Obj1.str = "HelloJava";  		// メンバ変数に値を代入
-      Obj1.Standard_out();  		// メソッドの実行
+   // サブクラスのインスタンス化を行う
+   public static void Hellojava_Class_method()
+   {
+     // Hellojava_Class_subをインスタンス化
+   　Hellojava_Class_sub Obj1 = new Hellojava_Class_sub() ;
+     // サブクラスのメソッド呼び出し
+     Obj1.Standard_out();
   }
 }
 
 // Hellojava_Class_sub.java
-class Hellojava_Class_sub {		// サブクラス
-    String str ;			// メンバ変数の定義
-    public Hellojava_Class_sub () {	// コンストラクタ
-	// (何もしない）
-    }
-    public void Standard_out() {		// メソッドの定義
-         System.out.println( str );		// 処理
-   }
+// サブクラス
+class Hellojava_Class_sub
+{
+  // コンストラクタ
+  Public Hellojava_Class_sub ()
+  {
+    // コンストラクタ (何もしない）
+  }
+  // メソッドの定義
+  public void Standard_out()
+  {
+    System.out.println("HelloJAVA");
+  }
 }
-
-
