f02e234155bbda204d47c23649bb392e136c4800
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-20 17:11:28 +0900

    Updata README

--- J0110_Output_Print/Output_print.java
+++ J0110_Output_Print/Output_print.java
@@ -21,9 +21,9 @@
 ### 文字列のリテラル出力をするメソッド
     void Output_String_lf ()
 ### 改行しないで文字列のリテラルの出力をするメソッド
-    void Output_String
+    void Output_String ()
 ### 文字列のリテラルを結合して出力するメソッド
-    void Output_Add
+    void Output_Add ()
 */
 
 // メインクラス
