d179720350b08632af8ec94adc68c9ea3b240ab3
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-20 19:53:11 +0900

    Update HelloJava_Base.java

--- J0010_HelloJava_Base/HelloJava_Base.java
+++ J0010_HelloJava_Base/HelloJava_Base.java
@@ -1,10 +1,26 @@
+/*
+# java HalloJava サンプル
+　初めてプログラミングをする方の為のベースになるサンプルプログラムです。
+
+# 解説
+　クラスHelloJava_Baseを作成します。
+  メインメソッドのみのプログラムです。
+
+## ソースファイル  
+   HelloJava_Base.java
+## メインクラス
+   HelloJava_Base
+## メソッド
+   無し
+*/
+
 // HelloJava_Base.java
 // 文字列[HelloJava]を標準出力する
 // メインクラスの宣言
 class HelloJava_Base {
-	//メインメソッドの宣言 
+	//メインメソッドの宣言
 	public static void main(String[] args)  {
-		//文の記述 
+		//文の記述
 		System.out.println("Hello JAVA!!") ;
     }
 }
