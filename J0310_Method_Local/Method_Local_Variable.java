4d903617e740a7746d3d92882c7ef43b935556c4
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-20 13:59:05 +0900

    local Variable

--- J0310_Method_Local/Method_Local_Variable.java
+++ J0310_Method_Local/Method_Local_Variable.java
@@ -1,21 +1,35 @@
-// Method_Local_Variable.java
 // Loacl変数：メソッド内で定義される変数
+// ローカル変数はそのメソッド内だけで有効になります。
+//
+// ソースファイル名
+//     Method_Local_Variable.java
+// メインクラス名
+//    Method_Local_Variable   
+// 整数のローカル変数宣言メソッド
+//       Variable_Int_Print () ;
+// 文字列のローカル変数宣言メソッド
+//       Variable_Str_Print ();
+// Method_Local_Variable.java
 class Method_Local_Variable {
     public static void main(String[] args) {
-	Variable_Int_Print () ;	// メソッド呼び出し
-	Variable_Str_Print ();	// メソッド呼び出し
+       // 整数のローカル変数宣言メソッド
+       Variable_Int_Print () ;
+       // 文字列のローカル変数宣言メソッド
+       Variable_Str_Print ();
   }
 
-   public static void Variable_Int_Print () {
-	int Var = 111  ;	// ローカル変数（整数）の変数初期化
-	System.out.print( "変数varの内容："  );
-	System.out.println( Var );
-   }
+  public static void Variable_Int_Print () {
+    // ローカル変数（整数）の変数初期化
+     int Var = 111  ;
+     System.out.print( "変数varの内容："  );
+     System.out.println( Var );
+  }
 
    public static void Variable_Str_Print () {
-	String	Str ;	// ローカル変数（文字列）の変数宣言
-	Str = "メインメソッド終了" ;
-	System.out.print   ( "変数strの内容："  );
-	System.out.println( Str );
+      // ローカル変数（文字列）の変数宣言
+      String	Str ;
+      Str = "メインメソッド終了" ;
+      System.out.print   ( "変数strの内容："  );
+      System.out.println( Str );
    }
 }
