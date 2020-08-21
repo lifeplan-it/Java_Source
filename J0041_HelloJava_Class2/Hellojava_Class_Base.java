27c9ba16d48047ad9833cdbe9f431ec0bdb4659f
waverlife <43716856+waverlife@users.noreply.github.com>
2020-08-21 17:06:35 +0900

    ReName BASE

--- J0041_HelloJava_Class2/Hellojava_Class_Base.java
+++ J0041_HelloJava_Class2/Hellojava_Class_Base.java
@@ -1,18 +1,27 @@
 // Hellojava_Class_Base.java
-class Hellojava_Class_Base {
-   public static void main(String[] args)   {
-      Hellojava_Class_sub Obj1 = new Hellojava_Class_sub() ; 	// Hellojava_Class_subをオブジェクト化
-      Obj1.str = "HelloJava";  		// メンバ変数に値を代入
-      Obj1.Standard_out();  		// メソッドの実行
-   }
+// メインクラス
+class Hellojava_Class_Base
+{
+  public static void main(String[] args)
+  {
+    // Hellojava_Class_subをインスタンス化
+    Hellojava_Class_sub Obj1 = new Hellojava_Class_sub() ;
+   // メンバ変数に値を代入
+    Obj1.str = "HelloJava";
+    // メソッドの実行
+    Obj1.Standard_out();
+  }
 }
 
 // Hellojava_Class_sub.java
+// サブクラス
 class Hellojava_Class_sub {
-    String str;			// メンバ変数の定義
-    public void Standard_out() {		// メソッドの定義
-         System.out.println( str );
-   }
+  // メンバ変数の定義
+  String str;
+  // メンバ変数の定義
+  public void Standard_out()
+  {
+    // メソッドの定義
+    System.out.println( str );
+  }
 }
-
-
